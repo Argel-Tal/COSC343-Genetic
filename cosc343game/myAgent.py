49ddc200680b15cdfac1fc69442ed07c802df19b
Jack <80669114+Argel-Tal@users.noreply.github.com>
2021-04-16 17:52:40 +1200

    Inital set up of chromosomes, and action func()

--- cosc343game/myAgent.py
+++ cosc343game/myAgent.py
@@ -1,4 +1,5 @@
 import numpy as np
+import random and random
 
 playerName = "myAgent"
 nPercepts = 75  #This is the number of percepts
@@ -7,15 +8,26 @@ nActions = 5    #This is the number of actionss
 # Train against random for 5 generations, then against self for 1 generations
 trainingSchedule = [("random", 5), ("self", 1)]
 
-# This is the class for your creature/agent
 
+# distance function
+# arrrayIndex is the 2 part position of a detected thing
+def manhattanDistance(arrayIndex, translatedX, translatedY):
+    distance = (abs(arrayIndex[0] - translatedX) + abs(arrayIndex[1] - translatedY))
+    return distance
+
+# This is the class for your creature/agent
 class MyCreature:
 
     def __init__(self):
         # You should initialise self.chromosome member variable here (whatever you choose it
         # to be - a list/vector/matrix of numbers - and initialise it with some random
         # values
-
+        self.weightAgent = random.randint(0,100)
+        self.weightAgentAttitude = random.randint(0,100)
+        self.weightFood = random.randint(0,100)
+        self.weightWall = random.randint(0,100)
+        self.weightConsume = random.randint(0,100)
+        self.chromosome = [self.weightAgent, self.weightAgentAttitude, self.weightFood, self.weightWall, weightConsume]
         # .
         # .
         # .
@@ -46,10 +58,32 @@ class MyCreature:
         # Different 'self.chromosome' should lead to different 'actions'.  This way different
         # agents can exhibit different behaviour.
 
-        # .
-        # .
-        # .
+        nets = np.zeros((nActions))
+        translations = [[-1,0],[0,-1],[+1,0],[0,+1]]
 
+        for i in range(nActions-1):
+            netOtherAgents = 0
+            netFoods = 0
+            netWalls = 0
+            currentDirection = translations[i]
+            agentsDetected = np.where(percepts[0] != 0) # assuming the tensor is indexable by map mode first
+            for agent in agentsDetected:
+                netOtherAgents += self.weightAgent * self.weightAgentAttitude * (manhattanDistance(agent, currentDirection[0], currentDirection[1]))
+
+            foodsDetected = np.where(percepts[1] != 0) # assuming the tensor is indexable by map mode first
+            for food in foodsDetected:
+                netFoods += self.weightFood * (manhattanDistance(food, currentDirection[0], currentDirection[1]))
+
+            wallsDetected = np.where(percepts[2] != 0) # assuming the tensor is indexable by map mode first
+            for wall in wallsDetected:
+                netWalls += self.weightWall * (manhattanDistance(wall, currentDirection[0], currentDirection[1]))
+
+            nets[i] = (netOtherAgents + netFoods + netWalls)
+
+        nets[4] = self.weightConsume
+
+        # set the value of 'actions', at the index corresponding to the index of 'nets' with the highest value, to 1
+        actions[nets.indexOf(max(nets))] = 1
         return actions
 
 def newGeneration(old_population):
