e2b8bf56e71fbd05b895beddbb97dcdd9420b119
Jack <80669114+Argel-Tal@users.noreply.github.com>
2021-05-19 17:00:13 +1200

    report updated, tweaks to other files are just extra runs

---
+++ COSC343-Genetic-main/cosc343game/defaults.py
@@ -0,0 +1,31 @@
+# You can manipulate these defaults to change the game parameters.
+
+game_params = {
+
+   #File implementing the agent playing as player 1 (exclude the .py extension)
+   "player1": "myAgent",
+
+   # File implementing the agent playing as player 2 (exclude the .py extension)
+   "player2": "randomPlayer",
+
+   # Speed of visualisation ('slow','normal','fast')
+   "visSpeed": 'normal',
+
+   # Visualisation resolution
+   "visResolution": (720, 480),
+
+   # Size of the game grid - default is 24
+   "gridSize": 24,
+
+   # Number of turns per game - default is 100
+   "nTurns": 100,
+
+   # Number of agents of each player - default is 34
+   "nAgents": 34,
+
+   # Number of walls in the grid - default is 20
+   "nWalls": 20
+
+}
+
+
